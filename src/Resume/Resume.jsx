b98ab974dbe54fc430025755a52ab81cdb23a3b6
Savon Huynh <xqhuynh@gmail.com>
2022-08-14 21:14:20 -0500

    Commit

--- src/Resume/Resume.jsx
+++ src/Resume/Resume.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Experience from "./Experience";
 
 function Resume() {
   return (
     <>
       <div className="resume-container">
-        <div>Resume</div>
+        <Experience />
       </div>
     </>
   );
